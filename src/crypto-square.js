ce8d8a89e1497b218865ee6276f5a6e0dee3be56
helloitsjoe <jboyle617@gmail.com>
2021-10-20 08:58:43 -0400

    crypto-square iteration 3

--- src/crypto-square.js
+++ src/crypto-square.js
@@ -9,13 +9,11 @@ class Crypto {
     const c = r * r >= cleaned.length ? r : r + 1;
     const padded = cleaned.padEnd(r * c);
 
-    const block = [...Array(r)].map((_, i) => padded.slice(i * c, i * c + c));
     const encrypted = Array(c).fill([]);
 
-    for (const row of block) {
-      for (const [i, letter] of Object.entries(row)) {
-        encrypted[i] += letter;
-      }
+    for (const [i, letter] of Object.entries(padded)) {
+      const row = i % c;
+      encrypted[row] += letter;
     }
 
     return encrypted.join(' ');
