fee69cadfef7b35eb73af0fcdadb2a8dd835f2e6
helloitsjoe <jboyle617@gmail.com>
2021-10-25 23:43:07 -0400

    WIP assembly line

---
+++ rust/src/exercises/assembly-line.rs
@@ -0,0 +1,17 @@
+mod numbers {
+  let HOURLY: u8 = 221
+  pub fn production_rate_per_hour(speed: u8) -> f64 {
+    (speed * HOURLY) as f64
+  }
+
+  pub fn working_items_per_minute(speed: u8) -> u32 {
+    let working_factor: f32 = match (speed) {
+      (1..=4) => 1.0,
+      (5..=8) => 0.9,
+      (9 | 10) => 0.77,
+      _ => 0.0
+    };
+
+    (working_factor * HOURLY as f32 * speed as f32) as u32
+  }
+}
