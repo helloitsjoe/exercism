0d3fb364bc95469eb1a173fb13e68a8e74f4b49f
helloitsjoe <jboyle617@gmail.com>
2021-11-06 16:04:03 -0400

    iteration

--- src/two-bucket.js
+++ src/two-bucket.js
@@ -1,63 +1,24 @@
 const LEFT = 'one';
 const RIGHT = 'two';
-
-const transfer = (from, to, toCapacity) => {
-  if (from === 0 || to === toCapacity) return [from, to];
-  return transfer(from - 1, to + 1, toCapacity);
-};
-
-const compute = (
-  leftCapacity,
-  rightCapacity,
-  goal,
-  start,
-  tuple,
-  moves = 1,
-  seen = {}
-) => {
-  let [leftAmount, rightAmount] = tuple;
-  if (leftAmount === goal || rightAmount === goal) return { tuple, moves };
-
-  const newTuple = (() => {
-    if (leftAmount === leftCapacity)
-      return transfer(leftAmount, rightAmount, rightCapacity);
-    if (!leftAmount) return [leftCapacity, rightAmount];
-    if (rightAmount === rightCapacity) return [leftAmount, 0];
-    if (!rightAmount) return transfer(leftAmount, rightAmount, rightCapacity);
-  })();
-
-  const seenKey = newTuple.toString();
-  if (seen[seenKey]) return { tuple: [], moves: -1 };
-
-  return compute(
-    leftCapacity,
-    rightCapacity,
-    goal,
-    start,
-    newTuple,
-    moves + 1,
-    { ...seen, [seenKey]: true }
-  );
-};
+const bucketNames = [LEFT, RIGHT];
 
 class TwoBucket {
-  constructor(leftCapacity, rightCapacity, goal, start) {
-    this.leftCapacity = leftCapacity;
-    this.rightCapacity = rightCapacity;
-    this.goal = goal;
-    this.start = start;
-    const startTuple = start === LEFT ? [leftCapacity, 0] : [rightCapacity, 0];
-    const leftBucket = start === LEFT ? leftCapacity : rightCapacity;
-    const rightBucket = start === LEFT ? rightCapacity : leftCapacity;
-    const { tuple, moves } = compute(
-      leftBucket,
-      rightBucket,
-      goal,
-      start,
-      startTuple
-    );
+  constructor(left, right, goal, start) {
+    const init = start === LEFT ? this.startLeft : this.startRight;
+    const { buckets, moves } = init(left, right, goal);
+
+    this.goalIndex = buckets.indexOf(goal);
     this._moves = moves;
-    this.tuple = start === LEFT ? tuple : tuple.reverse();
+    this.buckets = buckets;
+  }
+
+  startLeft(left, right, goal) {
+    return compute(left, right, goal, [left, 0]);
+  }
+
+  startRight(left, right, goal) {
+    const { buckets, moves } = compute(right, left, goal, [right, 0]);
+    return { buckets: buckets.reverse(), moves };
   }
 
   moves() {
@@ -66,13 +27,42 @@ class TwoBucket {
   }
 
   get goalBucket() {
-    const buckets = [LEFT, RIGHT];
-    return buckets[this.tuple.indexOf(this.goal)];
+    return bucketNames[this.goalIndex];
   }
 
   get otherBucket() {
-    return this.tuple.find(amount => amount !== this.goal);
+    return this.buckets[Number(!this.goalIndex)];
   }
 }
+
+const transfer = (from, to, toCapacity) => {
+  if (from === 0 || to === toCapacity) return [from, to];
+  return transfer(from - 1, to + 1, toCapacity);
+};
+
+const compute = (left, right, goal, buckets, moves = 1, seen = {}) => {
+  if (buckets.includes(goal)) {
+    return { buckets, moves };
+  }
+
+  const [leftAmount, rightAmount] = buckets;
+  const leftIsFull = leftAmount === left;
+  const rightIsFull = rightAmount === right;
+
+  const newPair = (() => {
+    if (rightIsFull) return [leftAmount, 0]; // Empty right bucket
+    if (!leftAmount) return [left, rightAmount]; // Fill left bucket
+    if (leftIsFull) return transfer(leftAmount, rightAmount, right);
+    if (!rightAmount) return transfer(leftAmount, rightAmount, right);
+  })();
+
+  // Cast array to string
+  if (seen[newPair]) return { buckets: [], moves: -1 };
+
+  return compute(left, right, goal, newPair, moves + 1, {
+    ...seen,
+    [newPair]: true,
+  });
+};
 
 module.exports = { TwoBucket, transfer };
