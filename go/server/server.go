0b057ad9045a6eccd452ca6192a61a7d62187c28
helloitsjoe <jboyle617@gmail.com>
2022-02-15 00:04:00 -0500

    serve stuff

--- go/server/server.go
+++ go/server/server.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func test(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "Testing...")
+}
+
+func headers(w http.ResponseWriter, req *http.Request) {
+	for name, headers := range req.Header {
+		for _, h := range headers {
+			fmt.Fprintf(w, "%v: %v/n", name, h)
+		}
+	}
+}
 
 func main() {
-	fmt.Println("TODO...")
+	http.HandleFunc("/test", test)
+	http.HandleFunc("/headers", headers)
+
+	fmt.Println("Listening on http://localhost:8080/test")
+
+	http.ListenAndServe(":8080", nil)
 }
