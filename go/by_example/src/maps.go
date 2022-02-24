a5a0c8f472f11ea204314981ad624e40b2f0eb22
helloitsjoe <jboyle617@gmail.com>
2022-02-24 08:50:08 -0500

    more maps

--- go/by_example/src/maps.go
+++ go/by_example/src/maps.go
@@ -11,4 +11,17 @@ func Maps() {
 	m["k2"] = 13
 
 	fmt.Println("map:", m)
+
+	v1 := m["k1"]
+	fmt.Println("v1:", v1)
+	fmt.Println("map length:", len(m))
+
+	delete(m, "k2")
+	fmt.Println("map:", m)
+
+	_, exists := m["k2"]
+	fmt.Println("exists:", exists)
+
+	n := map[string]string{"foo": "bar", "bar": "baz"}
+	fmt.Println("n:", n)
 }
