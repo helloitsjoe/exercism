d10cc42b51f1e68f59a7d39542f6bc7fa6e11c08
helloitsjoe <jboyle617@gmail.com>
2022-03-17 19:18:52 -0400

    more channels

---
+++ go/by_example/src/range-over-channels.go
@@ -0,0 +1,14 @@
+package by_example
+
+import "fmt"
+
+func RangeOverChannels() {
+  queue := make(chan string, 2)
+  queue <- "one"
+  queue <- "two"
+  close(queue)
+
+  for elem := range queue {
+    fmt.Println(elem)
+  }
+}
