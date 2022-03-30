65988e2c94340806e5bbceb6d43446e113cd22ea
helloitsjoe <jboyle617@gmail.com>
2022-03-30 14:33:44 -0400

    tests pass

--- go/exercism/tournament/tournament.go
+++ go/exercism/tournament/tournament.go
@@ -2,6 +2,7 @@
 package tournament
 
 import (
+	"errors"
 	"fmt"
 	"io"
 	"sort"
@@ -18,6 +19,12 @@ type formattedRecord struct {
 }
 
 type record = map[string]int
+
+var validOutcomes = map[string]bool{
+	"win":  true,
+	"loss": true,
+	"draw": true,
+}
 
 func parseReader(reader io.Reader) (string, error) {
 	b := make([]byte, 8)
@@ -37,32 +44,40 @@ func parseReader(reader io.Reader) (string, error) {
 }
 
 func convertToWrite(s string) ([]byte, error) {
-	b := make([]byte, 8)
+	b := make([]byte, 0)
 	for _, char := range s {
 		b = append(b, byte(char))
 	}
 	return b, nil
 }
 
-func getRecords(s string) map[string]record {
+func getRecords(s string) (map[string]record, error) {
 	games := strings.Split(s, "\n")
 	records := make(map[string]record)
 
 	for _, game := range games {
-		// Why is this empty sometimes?
-		if game == "" {
+		if game == "" || game == " " || game[0] == '#' {
 			continue
 		}
 
 		r := strings.Split(game, ";")
 
+		if len(r) != 3 {
+			return nil, errors.New("Game should have two teams and an outcome")
+		}
+
 		firstTeam := r[0]
 		secondTeam := r[1]
 		firstOutcome := r[2]
 
+		if _, ok := validOutcomes[firstOutcome]; !ok {
+			return nil, errors.New("Unrecognized outcome")
+		}
+
 		var secondOutcome string
 		var firstPoints int
 		var secondPoints int
+
 		if firstOutcome == "win" {
 			firstPoints = 3
 			secondPoints = 0
@@ -107,7 +122,7 @@ func getRecords(s string) map[string]record {
 		records[secondTeam] = secondRecord
 	}
 
-	return records
+	return records, nil
 }
 
 func formatTable(r map[string]record) string {
@@ -118,18 +133,23 @@ func formatTable(r map[string]record) string {
 	}
 
 	sort.SliceStable(toSort, func(i, j int) bool {
-		return int(toSort[i].points) > int(toSort[j].points)
+		firstPoints := int(toSort[i].points)
+		secondPoints := int(toSort[j].points)
+
+		if firstPoints == secondPoints {
+			return toSort[i].name < toSort[j].name
+		}
+
+		return firstPoints > secondPoints
 	})
 
-	teams := []string{fmt.Sprintf("%-30s | %2s | %2s | %2s | %2s | %2s", "Team", "MP", "W", "D", "L", "P")}
+	teams := []string{fmt.Sprintf("%-30s | %2s | %2s | %2s | %2s | %2s\n", "Team", "MP", "W", "D", "L", "P")}
 	for _, v := range toSort {
-		team := fmt.Sprintf("%-30s | %2d | %2d | %2d | %2d | %2d", v.name, v.matches, v.win, v.draw, v.loss, v.points)
+		team := fmt.Sprintf("%-30s | %2d | %2d | %2d | %2d | %2d\n", v.name, v.matches, v.win, v.draw, v.loss, v.points)
 		teams = append(teams, team)
 	}
-	teams = append(teams, "\n")
 
-	// Join slices
-	return strings.Join(teams, "\n")
+	return strings.Join(teams, "")
 }
 
 // Tally does the tallying
@@ -138,12 +158,21 @@ func Tally(reader io.Reader, writer io.Writer) error {
 	if err != nil {
 		return err
 	}
-	records := getRecords(s)
-	output := formatTable(records)
 
-	// fmt.Println(records)
-	// fmt.Println(output)
+	records, err := getRecords(s)
+
+	if err != nil {
+		return err
+	}
+
+	output := formatTable(records)
+	fmt.Println("output:", output)
 	bytes, err := convertToWrite(output)
+
+	if err != nil {
+		return err
+	}
+
 	writer.Write(bytes)
 	return nil
 }
