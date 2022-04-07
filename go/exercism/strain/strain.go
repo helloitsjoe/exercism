f3940b64085771a22920d71f24b4332d9eb08ec8
helloitsjoe <jboyle617@gmail.com>
2022-04-06 22:52:08 -0400

    strain with generics

--- go/exercism/strain/strain.go
+++ go/exercism/strain/strain.go
@@ -5,52 +5,45 @@ type Lists [][]int
 type Strings []string
 
 // Generics in go 1.18!
-// func keep[T comparable](filter func(T) bool, input []T) T {
-// 	if input == nil {
-// 		return nil
-// 	}
-// 	toKeep := make([]T, 0, len(input))
-// 	for _, item := range input {
-// 		if filter(item) {
-// 			toKeep = append(toKeep, item)
-// 		}
-// 	}
-// 	return toKeep
-// }
-//
-// func (i Ints) Keep(filter func(int) bool) Ints {
-// 	return keep[int](filter, i)
-// }
-
-func (i Ints) Discard(filter func(int) bool) Ints {
-	if i == nil {
+func keep[T any](filter func(T) bool, input []T) []T {
+	if input == nil {
 		return nil
 	}
-	toDiscard := make(Ints, 0, len(i))
-	for _, num := range i {
-		if !filter(num) {
-			toDiscard = append(toDiscard, num)
+
+	toKeep := make([]T, 0, len(input))
+	for _, item := range input {
+		if filter(item) {
+			toKeep = append(toKeep, item)
+		}
+	}
+	return toKeep
+}
+
+func discard[T any](filter func(T) bool, input []T) []T {
+	if input == nil {
+		return nil
+	}
+	toDiscard := make([]T, 0, len(input))
+	for _, item := range input {
+		if !filter(item) {
+			toDiscard = append(toDiscard, item)
 		}
 	}
 	return toDiscard
 }
 
+func (i Ints) Keep(filter func(int) bool) Ints {
+	return keep[int](filter, i)
+}
+
+func (i Ints) Discard(filter func(int) bool) Ints {
+	return discard[int](filter, i)
+}
+
 func (l Lists) Keep(filter func([]int) bool) Lists {
-	toKeep := make(Lists, 0, len(l))
-	for _, list := range l {
-		if filter(list) {
-			toKeep = append(toKeep, list)
-		}
-	}
-	return toKeep
+	return keep[[]int](filter, l)
 }
 
 func (s Strings) Keep(filter func(string) bool) Strings {
-	toKeep := make(Strings, 0, len(s))
-	for _, list := range s {
-		if filter(list) {
-			toKeep = append(toKeep, list)
-		}
-	}
-	return toKeep
+	return keep[string](filter, s)
 }
