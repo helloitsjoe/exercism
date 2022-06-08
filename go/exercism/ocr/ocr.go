37e5c7bf9fee20753060aeb3b4fc89be1f12d65a
helloitsjoe <jboyle617@gmail.com>
2022-06-08 19:39:20 -0400

    ocr

---
+++ go/exercism/ocr/ocr.go
@@ -0,0 +1,94 @@
+package ocr
+
+import (
+// "fmt"
+// "strings"
+)
+
+func recognizeDigit(input string) string {
+	const zero = `
+ _ 
+| |
+|_|
+   `
+
+	const one = `
+   
+  |
+  |
+   `
+
+	const two = `
+ _ 
+ _|
+|_ 
+   `
+
+	const three = `
+ _ 
+ _|
+ _|
+   `
+
+	const four = `
+   
+|_|
+  |
+   `
+
+	const five = `
+ _ 
+|_ 
+ _|
+   `
+
+	const six = `
+ _ 
+|_ 
+|_|
+   `
+
+	const seven = `
+ _ 
+  |
+  |
+   `
+
+	const eight = `
+ _ 
+|_|
+|_|
+   `
+
+	const nine = `
+ _ 
+|_|
+ _|
+   `
+
+	mapper := make(map[string]string)
+	mapper[zero] = "0"
+	mapper[one] = "1"
+	mapper[two] = "2"
+	mapper[three] = "3"
+	mapper[four] = "4"
+	mapper[five] = "5"
+	mapper[six] = "6"
+	mapper[seven] = "7"
+	mapper[eight] = "8"
+	mapper[nine] = "9"
+
+	val := mapper[input]
+
+	if val == "" {
+		return "?"
+	}
+
+	return val
+}
+
+func Recognize(input string) []string {
+	// TODO: Split string into 3x4 digits
+	digit := recognizeDigit(input)
+	return []string{digit}
+}
